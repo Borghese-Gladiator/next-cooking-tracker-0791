858ebc6c80d8ed21102e9389495d82835bce249c
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2023-05-21 16:10:14 -0400

    fix: look/feel of cards

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,29 +36,21 @@ Home.getInitialProps = async (ctx) => {
 };
 
 
-const Card = (props) => {
-  const { thumbnail, name, createdAt, updatedAt } = props;
+const Card = ({ thumbnail, name, createdAt, updatedAt }) => {
   console.log('updatedAt', updatedAt);
   console.log('thumbnail', thumbnail);
   const createdAtDate = new Date(createdAt);
   const updatedAtDate = new Date(updatedAt);
   return (
     <div className={styles.card}>
-      {thumbnail && <img className={styles.cardThumbnail} src={thumbnail} alt="thumbnail" />}
-      <div className={styles.cardBody}>
-        <h2>{name}</h2>
-        <p className={styles.cardSecondary}>{`Created At: ${formatDate(createdAtDate)}`}</p>
-        <p className={styles.cardSecondary}>{`Updated At: ${formatDate(updatedAtDate)}`}</p>
+      {thumbnail && <img src={thumbnail} alt="thumbnail" />}
+      <h2>{name}</h2>
+      <div className={styles.dateCaption}>
+        <h4>{`${formatDate(createdAtDate)}`}</h4>
+        <span>{`Last Updated: ${formatDate(updatedAtDate)}`}</span>
       </div>
     </div>
   );
 };
-
-/*
-Card.PropTypes = {
-  thumbnail: string, // path to asset in /public
-}
-*/
-
 
 export default Home;
